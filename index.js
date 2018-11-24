c97ff20d4e8c167a1438672b2c86f66bbb289310
github username <chao.alexander.123@gmail.com>
2018-11-24 16:33:48 -0500

    test fetch with authorization token in header

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much.";
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits');
+// xhr.responseType = 'json';
+//
+// xhr.onload = function() {
+//   console.log(xhr.response);
+// };
+//
+// xhr.onerror = function() {
+//   console.log('Booo');
+// };
+//
+// xhr.send();
+
+const token = '506c4d24c80938131adb31820ee3fed9b0aa7cb7'
+fetch('https://api.github.com/users/alexanderchao123/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
